45f72ab18f1d6db5f86da67bc319292e43afeb2d
Seyeon Park <96575761+yeonselily@users.noreply.github.com>
2026-01-25 10:27:27 -0800

    first commit

---
+++ css475php/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MKochanski
+ * Date: 7/24/2018
+ * Time: 2:47 PM
+ */
+$servername = 'seyeon.vergil.u.washington.edu'; // Your Server (if noot)
+$username = 'seyeon';
+$password = 'Staur083';
+$database = 'point_of_sale';
+$port = 1024; // Your Port
